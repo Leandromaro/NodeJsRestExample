5be13b25f6bd60c60c97a56068869dfcb11f4e27
leandromaro <leandro.maro@globant.com>
2017-05-01 11:39:12 -0300

    uploading first files

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+})
+
+app.get('/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       users = JSON.parse( data );
+       var user = users["user" + req.params.id] 
+       console.log( user );
+       res.end( JSON.stringify(user));
+   });
+})
+
+app.delete('/deleteUser/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       var id = req.params.id;
+       data = JSON.parse( data );
+       delete data["user" + id];
+       console.log( data );
+       res.end( JSON.stringify(data));
+   });
+})
+
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
