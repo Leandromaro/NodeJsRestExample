442c08fc3c47c53c541f0bb1161c5c5bcbc9c24f
leandromaro <leandro.maro@globant.com>
2017-08-08 23:50:16 -0300

    creating a regex service with node and express

--- server.js
+++ server.js
@@ -2,39 +2,15 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-//In the above case require('events'); would return an Object containing EventEmitter
-//require returns exactly what you put in module.exports
-
-app.get('/listUsers', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+app.get('/allExpresions', function (req, res) {
+   fs.readFile( __dirname + "/" + "regex.json", 'utf8', function (err, data) {
        console.log( data );
+       res.set('Content-Type', 'application/json');
        res.end( data );
    });
 })
 
-app.get('/:id', function (req, res) {
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       users = JSON.parse( data );
-       var user = users["user" + req.params.id] 
-       console.log( user );
-       res.end( JSON.stringify(user));
-   });
-})
-
-app.delete('/deleteUser/:id', function (req, res) {
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       var id = req.params.id;
-       data = JSON.parse( data );
-       delete data["user" + id];
-       console.log( data );
-       res.end( JSON.stringify(data));
-   });
-})
-
-
-var server = app.listen(8081, function () {
+var server = app.listen(8181, function () {
 
   var host = server.address().address
   var port = server.address().port
